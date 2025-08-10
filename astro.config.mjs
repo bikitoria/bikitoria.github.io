d2243d5b178786757c66159094f9e2d7899c2974
Victoria Piquer <victoriapiquer.3@gmail.com>
2025-08-10 10:02:01 -0600

    Initial Astro site

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://github.com/bikitoria/bikitoria.github.io.git/',
+  base: '/', // for project pages, remove for username.github.io site
+});
