c2ced3909be9122ff5cd1ef670bbc975e9425916
Victoria Piquer <victoriapiquer.3@gmail.com>
2025-08-10 19:08:40 -0600

    Deploy latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://bikitoria.github.io',
   base: '/',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
